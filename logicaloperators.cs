5acfb64d6b04ddeb625d94a115c85411cd24c5a2
Philip-Loeffler <Philiploeffler@gmail.com>
2018-11-26 12:32:38 -0500

    origin

---
+++ logicaloperators.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3; 
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a / b);
+            // just like in math, some operators take precidence over others
+            Console.WriteLine(a + b * c);
+            // boolean expression will always render a boolean value
+            Console.WriteLine( a == b );
+
+            // tricky example, two negatives always equal a positive
+     
+            Console.WriteLine(!(a != b));
+
+            //  c is greater than be and c is greater than a meaning this will result in a true
+
+            Console.WriteLine(c > b && c > a);
+
+            // and a true and false will result in false 
+            Console.WriteLine(c > b && c == a );
+
+        }
+    }
+}
---
+++ typeconversion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typeconversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+            int l = 1;
+            // cannot implicity conver type int to byte
+            byte v = l;
+            // casting so no data is lost 
+            byte m = (byte)l;
+            Console.WriteLine(l);
+            // now non compatible types
+            string number = "1234";
+            //cannot convert int to string so need to use convert class
+            int a = Convert.ToInt32(number);
+            Console.WriteLine(a);
+            // crashing the system now, trying to convert string with larger number than byte
+          
+            byte f = Convert.ToByte(number);
+            Console.WriteLine(f);
+
+            // last using an try catch block to run the code 
+            // since we know the byte cannot be converted
+            // the code tries to compile and run the convert but it doesnt work
+            // so it catches and throws the console right line exception
+                 try
+            {
+                var numbers = "122334";
+                byte z = Convert.ToByte(numbers);
+                Console.WriteLine(z);
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("the number could not be converted to a byte");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
