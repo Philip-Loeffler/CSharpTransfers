33a89e6bdf18e41c01f13d8fed48ee98da664a7b
Philip-Loeffler <Philiploeffler@gmail.com>
2018-11-28 12:33:48 -0500

    origin

---
+++ randomclass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // random class gives us random integers
+            var random = new Random();
+            for (var i = 0; i <=10; i++)
+                // this will give us random numbers between one and ten
+                Console.WriteLine(random.Next(1, 10));
+
+            // fun fact, computers only understand numbers
+            // here is a lowercase a's binary number
+            Console.WriteLine((int)'a');
+            // here is a random string of chars
+            Console.Write((char)random.Next(0, 26));
+
+            // last creating a random string for a password
+
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (var a = 10; a <= 10; a++)
+                buffer[a] = (char)('a' + random.Next(0, 26));
+            var password = new string(buffer);
+            Console.WriteLine(password);
+       
+
+        }
+    }
+}
---
+++ whileloops.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            //same increment code as for loop
+            var i = 0;
+            while (i <= 10)
+                {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+
+                i++;
+                }
+            // this is an infinte while loop without anything read for console
+            while (true)
+            {
+                Console.Write("type your name");
+                // here is letting the user put in something
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                // after putting name it just literally just repeats whatever input 
+                Console.WriteLine("@echo:" + input);
+            }
+        }
+    }
+}
